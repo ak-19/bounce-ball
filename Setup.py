05e159d935b24f16a725fbd6455fa7f37b50e929
ante <ante.kotarac@gmail.com>
2022-01-05 22:08:33 +0100

    custom mouse

--- Setup.py
+++ Setup.py
@@ -7,7 +7,8 @@ class Setup:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('Bounce the ball')
-        
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                        
     def get_display(self):
         return pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
 
